57305175595d9e7aaaad0a9fbbc7653ed5195edc
Daniel Toledo Moreno <danieltoledo790@gmail.com>
2021-10-11 01:37:11 -0500

    prueva1002

--- servidor.py
+++ servidor.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import date, datetime
 
 app = Flask(__name__)
+PORT=5000
+DEBUG=False
 # 'postgresql://<usuario>:<contraseña>@<direccion de la db>:<puerto>/<nombre de la db>
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/tiendadb'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cpdxxdhbrovthw:25d3ec47451f8e836675f3227c1713ee6ad8a0319f81895a5f666d4363029d46@ec2-52-207-47-210.compute-1.amazonaws.com:5432/d80ef8qqjqdhd0'
@@ -263,5 +265,5 @@ def get_facturar():
         
         return render_template("facturar.html", factura=factura, ventas=ventas, producto=producto)
  
-"""if __name__ == "__main__":
-    app.run()#debug=True)"""
+if __name__ == "__main__":
+    app.run(port=PORT,debug=DEBUG)
--- testserver.py
+++ testserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+"""from flask import Flask
 
 app = Flask(__name__)
 PORT=5000
@@ -6,7 +6,7 @@ DEBUG=False
 
 @app.route('/')
 def home():
-    return "Comprobando que funciona el deploy"
+    return "Comprobando que funciona el deploy" """
 
-if __name__ == "__main__":
-    app.run(port=PORT,debug=DEBUG)
+"""if __name__ == "__main__":
+    app.run(port=PORT,debug=DEBUG)"""
